ea041812f4b5f1849f272b781b32fd84b9aea47e
Phil Tietjen <tietjen.philip@gmail.com>
2019-02-11 23:44:51 -0500

    create build folder

---
+++ build/precache-manifest.513e0c958846f8e8888cb430f494546a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15abad3fb5ea3e7b5017",
+    "url": "/static/css/main.b66a3ecb.chunk.css"
+  },
+  {
+    "revision": "15abad3fb5ea3e7b5017",
+    "url": "/static/js/main.15abad3f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a7301e2a5c951b4238d8",
+    "url": "/static/js/2.a7301e2a.chunk.js"
+  },
+  {
+    "revision": "a9b0be2de8a9eff30960003409e8bb24",
+    "url": "/index.html"
+  }
+];
